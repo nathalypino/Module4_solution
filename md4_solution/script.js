b3abbfab2bb67494422509d7b0764e6099cd072f
Nathaly Pino <155981420+nathalypino@users.noreply.github.com>
2024-03-28 02:53:07 +0100

    solution script.js

--- md4_solution/script.js
+++ md4_solution/script.js
@@ -21,47 +21,42 @@ Hello Paula
 Hello Laura
 Good Bye Jim
 
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-
 */
 
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// *******************************
 // Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
+// *******************************
+
 (function () {
+  
+  var helloSpeaker = {
+    speak: function(name) {
+      console.log("Hello " + name);
+      document.write("<p>Hello " + name + "</p>"); 
+    }
+  };
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+ 
+  var byeSpeaker = {
+    speak: function(name) {
+      console.log("Good Bye " + name);
+      document.write("<p>Good Bye " + name + "</p>"); 
+    }
+  };
 
-// STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = names[i].charAt(0).toLowerCase();
+  // Loop 
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase(); 
 
-  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
+    
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
   }
-}
 
 })();
+
